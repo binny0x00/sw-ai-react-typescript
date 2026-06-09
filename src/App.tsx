56ee1bbe316e42382d957a6aa11337fc3fe0ec25
binny0x00 <binny0x00@gmail.com>
2026-06-10 03:49:30 +0900

    feat: 문의 게시판 화면 추가 (#3)
    * feat: 문의 게시판 목록 화면
    
    * feat: 문의 작성, 문의 상세 화면

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { AppLayout } from './app/AppLayout'
 import { AuthPage } from './pages/auth/AuthPage'
 import { HomePage } from './pages/home/HomePage'
+import { InquiryDetailPage } from './pages/inquiries/InquiryDetailPage'
+import { InquiryWritePage } from './pages/inquiries/InquiryWritePage'
 import { InquiriesPage } from './pages/inquiries/InquiriesPage'
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Route element={<AppLayout />}>
         <Route index element={<HomePage />} />
         <Route path="inquiries" element={<InquiriesPage />} />
+        <Route path="inquiries/new" element={<InquiryWritePage />} />
+        <Route path="inquiries/:inquiryId" element={<InquiryDetailPage />} />
       </Route>
       <Route path="auth" element={<AuthPage />} />
       <Route path="*" element={<Navigate replace to="/" />} />
